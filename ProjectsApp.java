9bc16192d84e219794e84c4aaa2841ca2efa7b9a
pandi <pandiebear02@gmail.com>
2023-01-18 17:42:46 -0800

    pushed code files

---
+++ ProjectsApp.java
@@ -0,0 +1,13 @@
+package projectsM;
+
+import projects.dao.DbConnections;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+
+		DbConnections.getConnection(); 
+		
+	}
+
+}
